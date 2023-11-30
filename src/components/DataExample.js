bb906bc843f37639e54dc9aae0eaa5d0c41cd0cf
WarManDev <warman2015@gmail.com>
2023-11-30 01:45:12 -0600

    First commit

---
+++ src/components/DataExample.js
@@ -0,0 +1,25 @@
+import React from "react";
+import useJsonFetch from "../hooks/useJsonFetch";
+import PropTypes from "prop-types";
+
+export default function DataExample({ endpoint, title }) {
+  const [data, error, loading] = useJsonFetch(
+    `http://localhost:7070/${endpoint}`
+  );
+
+  return (
+    <div>
+      <span>
+        {title}:{" "}
+        {(loading && "Загрузка...") ||
+          (data && data.status) ||
+          (error && "Ошибка!")}
+      </span>
+    </div>
+  );
+}
+
+DataExample.propTypes = {
+  endpoint: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
